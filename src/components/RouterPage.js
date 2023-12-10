3a8d582b2510f88c804e42e5532e3eda16d8ff95
Dilsha <dilsha.pamuditha@app360.lk>
2023-12-10 19:57:32 +0530

    emedical_FE-Master

---
+++ src/components/RouterPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+
+export default function RouterPage(){
+    return(
+        <Router>
+            <Routes>
+                {/* <Route path='/' element={}/> */}
+            </Routes>
+        </Router>
+    )
+}
